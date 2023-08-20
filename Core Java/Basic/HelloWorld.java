36b613628301e061db93fc642451d2ed82e1f774
ankit_rawat <ankitrawat741@gmail.com>
2023-08-20 20:09:39 +0530

    Java:history

---
+++ Core Java/Basic/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.lang.*;		//imported by default
+ class HelloWorld{
+	
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+	}
+}
+
+ class HelloWorld1{
+	
+	public static void main(String args[]){
+		System.out.println("Hello1 World!");
+	}
+}
